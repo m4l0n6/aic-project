e9ca6d27c73baa08ca3d34212487397e112372d3
Long Ma <mahonglonghg123@gmail.com>
2025-09-17 15:08:31 +0700

    fix deploy

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority"
-import type { VariantProps } from "class-variance-authority/types";
+import type { VariantProps } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
